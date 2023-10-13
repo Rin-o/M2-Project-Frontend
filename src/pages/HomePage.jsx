f16a90bd9323580851158627dc7bcc68040814e8
Rin-o <rino091370@gmail.com>
2023-10-13 02:20:43 +0200

    Alice's original code on the Update page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import cloudImage from '../assets/Images/cloud_icon.png'
 import nannyImage from '../assets/Images/full-shot-teacher-helping-girl-learn.jpg'
 
@@ -11,7 +12,11 @@ const HomePage = () => {
                 <p style={{paddingTop: '4rem', lineHeight:'2em'}}>With warm, experienced nannies, full-day schedules,
                     <br />rolling admissions and a world-class curriculum,
                     <br />IronNanny meets you where you are.</p>
+                <Link to='/babysitters'>
+                    <button className="btnNav">Find nannies near you that you’ll love</button>
+                </Link><br/>
                 <img src={cloudImage} alt="cloud icon" className="cloud"/>
+
                 </div>
                 <img src={nannyImage} alt="Image a teacher helping a child learn" className="imageRectangle"/>
             </div>
